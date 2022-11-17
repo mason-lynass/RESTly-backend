b311a96d728a7539530ce4090ecacc2642105b90
Mason <masonlynass@gmail.com>
2022-11-16 21:22:27 -0800

    using the specific get again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,26 +7,17 @@ class ApplicationController < Sinatra::Base
     restaurants.to_json(include: [:reviews, :favorites])
   end
 
-  # #GET Request for a specific restaurant, its reviews, and its favorites
-  # get "/restaurants/:id" do
-  #   restaurant = Restaurant.find(params[:id])
-  #   restaurant.to_json(include: [:reviews, :favorites])
-  # end
+  #GET Request for a specific restaurant, its reviews, and its favorites
+  get "/restaurants/:id" do
+    restaurant = Restaurant.find(params[:id])
+    restaurant.to_json(include: [:reviews, :favorites])
+  end
 
   #DELETE Request for a review
   delete '/reviews/:id' do
     review = Review.find(Params[:id])
     review.destroy
     review.to_json
-  end
-
-  #POST Request for a new item
-  post '/favorites' do
-    favorite = Favorite.create(
-      food_name: params[:food_name],
-      likes: params[:likes],
-      restaurant: params[:restaurant]
-    )
   end
 
   #PATCH Request for a review
@@ -37,4 +28,23 @@ class ApplicationController < Sinatra::Base
       review: params[:review]
     )
   end
+
+  #POST Request for a new favorite
+  post '/favorites' do
+    favorite = Favorite.create(
+      food_name: params[:food_name],
+      likes: params[:likes],
+      restaurant_id: params[:restaurant_id]
+    )
+  end
+
+  #PATCH Request for adding likes to a favorite
+  patch '/favorites/:id' do
+    favorite = Favorite.find(params[:id])
+    favorite.update(
+      likes: params[:likes],
+    )
+  end
+
+  
 end
